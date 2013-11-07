f7131a6de24b133a7f65e7e0b7a70b4411a74e5f
Jakob Höper <jakobh@dariahst.(none)>
2013-11-07 19:21:09 +0100

    more decipherable output.

--- rdf_foaf.py
+++ rdf_foaf.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*- 
 import rdflib
 from rdflib_sqlalchemy.SQLAlchemy import SQLAlchemy
 
@@ -20,9 +20,32 @@ print "number of predicates", len(g)
 print "namespaces:"
 for ns in g.namespaces():
 	print ns
-#print list(g)[:2]
-for s,p,o in g.triples((None, rdflib.RDF.type, None)):
-	print s,o
 
-for s,o in g.subject_objects(rdflib.RDF.type):
-	print s,o
+# sammel types ein:
+types={}
+for s,p,o in g.triples((None, rdflib.RDF.type, None)):
+	#print s,o
+	instances=types.get(o, [])+[s]
+	types[o] = instances
+
+# dasselbe in grün:
+#for s,o in g.subject_objects(rdflib.RDF.type):
+	#print s,o
+
+# printe typen und ausprägungen
+for t,ii in types.items():
+	cap="Instances of type {}:".format(t)
+	print "\n{}\n{}".format(cap, '-'*len(cap))
+	for n,i in enumerate(ii):
+		print "{}. {}".format(n+1,i)
+
+print "\nAha. Who is Nathan?"
+print "-------------------"
+print "\n".join(["{} {}".format(p,o) 
+				for s,p,o in g 
+				if str(s) == "http://collectivesource.com/foaf.rdf#nathan"])
+
+print "Write xml dump to foaf.rdf"
+xmldumpfile=open("foaf.rdf", "w")
+xmldumpfile.write(g.serialize())
+xmldumpfile.close()
