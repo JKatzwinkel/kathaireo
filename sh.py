e0cef40556ac076e4e96263868514bc8be6f86e5
Jakob Höper <jakobh@dariahst.(none)>
2013-11-11 18:14:42 +0100

    naja

---
+++ sh.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+from getch import getch
+import rdflib
+from rdflib_sqlalchemy.SQLAlchemy import SQLAlchemy
+
+
+#https://pypi.python.org/pypi/getch
+
+history=[]
+
+inputStr=""
+buffer=None
+
+def recvInput():
+	global inputStr
+	global buffer
+	ch=getch()
+	print ch
+	if ord(ch) == 10:
+		print "\r{}".format(inputStr)
+		ret=inputStr
+		inputStr=""
+		return ret
+	elif ord(ch) == 27:
+		buffer=[27]
+	elif buffer:
+		buffer.append(ord(ch))
+	else:
+		inputStr+=ch
+	print "\r{}".format(inputStr), ord(ch),
+	return None
+
+while True:
+	input = recvInput()
