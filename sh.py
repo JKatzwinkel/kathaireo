67d4161318ddebc9d55fa97966b5e50642c7d8c0
J. Katzwinkel <thor@mintj>
2013-11-14 13:40:26 +0100

    stuffing prompter class

--- sh.py
+++ sh.py
@@ -33,37 +33,47 @@ for k,f in combos:
 
 #https://pypi.python.org/pypi/getch
 
+
 class Prompter:
 	def __init__(self):
 		self.history=[]
-		self.buffer=None
+		self.buf=[]
+		self.keylvl=keypaths
 		self.currIn=""
 		self.suggestions=[]
 
+	def waitForInput(self):
+		ch = getch()
+		if ord(ch) in self.keylvl:
+			self.buf.append(ord(ch))
+			self.keylvl=self.keylvl[ord(ch)]
+		else:
+			self.keylvl=keypaths
+			self.buf=[]
+		if len(self.buf)<1:
+			print ch
 
-history=[]
 
-inputStr=""
-buffer=None
+	def onCombo(ch):
+		level=keypaths
+		if not buf:
+			globals()["buf"] = [ord(ch)]
+		for k in buf:
+			if type(level) is dict:
+				level = level.get(k)
+		if not level:
+			globals()["buf"] = None
+			return False
+		if type(level) != dict:
+			globals()["buf"] = None
+		else:
+			buf.append(ord(ch))
+		return True
 
-def recvInput():
-	global inputStr
-	global buffer
-	ch=getch()
-	print ch
-	if ord(ch) == 10:
-		print "\r{}".format(inputStr)
-		ret=inputStr
-		inputStr=""
-		return ret
-	elif ord(ch) == 27:
-		buffer=[27]
-	elif buffer:
-		buffer.append(ord(ch))
-	else:
-		inputStr+=ch
-	print "\r{}".format(inputStr), ord(ch),
-	return None
 
-#while True:
-	#input = recvInput()
+
+
+prompt = Prompter()
+
+while True:
+	inp = prompt.waitForInput()
