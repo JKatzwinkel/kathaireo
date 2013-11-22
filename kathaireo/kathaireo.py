dbab99cd873612c09970debfb1600e83a45cb286
Jakob Höper <jakobh@dariahst.(none)>
2013-11-23 00:53:26 +0100

    rdf module must be able to download/parse ontologies from the internet..

--- kathaireo/kathaireo.py
+++ kathaireo/kathaireo.py
@@ -46,3 +46,12 @@ if __name__=='__main__':
 #TODO: handle command line parameters in here.
 #TODO: bash autocompletion
 #TODO: default command registration in interactive mode
+#TODO: parsing of remote resources (https://kask.eti.pg.gda.pl/redmine/projects/sova/repository/revisions/00951bd8e28d7bd58facb5a1da3a17ae9df115d4/raw/portalSubsystem/data/pizza.owl)
+#TODO: somehow wrap line if input gets too long.
+
+#try to parse these ontologies from the internet:
+# https://owlverbalizer.googlecode.com/hg-history/8da67288d6be51f8d635f3bb347bacb1cf74f811/examples/tests.owl
+# http://smi-protege.stanford.edu/repos/protege/protege4/libraries/owlapi-extensions/trunk/src/test/resources/pizza01.owl
+# http://wise.vub.ac.be/ontologies/restaurant.owl
+
+
