aef3ffd69a1ae0306439c793d547588e70c4db71
J. Katzwinkel <thor@mintj>
2013-11-25 02:26:03 +0100

    Changed import statements to relative module paths hoping this would let sphinx find everything it looks for. This appeared to require to also move main module kathaireo.py up one level, out of kathaireo package. After doing all this update, all imports work, even in sphinx.
    Because sphinx is not that complicated after all, I began modifying
    existing docstrings to work with sphinx, which right now seems fairly
    satisfied and produces already pretty nice html output, which is
    located at docs/_build and included in this commit.

---
+++ docs/_build/html/_sources/kathaireo.txt
@@ -0,0 +1,20 @@
+kathaireo Package
+=================
+
+:mod:`kathaireo` Package
+------------------------
+
+.. automodule:: kathaireo.__init__
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Subpackages
+-----------
+
+.. toctree::
+
+    kathaireo.commands
+    kathaireo.rdf
+    kathaireo.shell
+
---
+++ kathaireo.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+"""Dokudoku!"""
+__docformat__ = "restructuredtext en"
+__version__ = "0.0.2b-dev"
+__author__ = "Dariah-DE"
+
+import getopt
+
+from kathaireo import shell
+
+def main():
+	import html5lib
+	import sqlalchemy
+	welcome=[
+		'This is {}, version "{}".'.format(__file__, __version__),
+		' rdflib version: {}'.format(shell.rdf.rdflib.__version__),
+		' rdflib_sqlalchemy version: {}'.format(
+		shell.rdf.storage.rdflib_sqlalchemy.__version__),
+		' sqlalchemy version: {}'.format(sqlalchemy.__version__),
+		' html5lib version: {}'.format(html5lib.__version__),
+		' urllib2 version: {}'.format(shell.rdf.remote.urllib2.__version__),
+		'stats:',
+		' default commands ready: {}'.format(
+		len(shell.commands.cmdict)),
+		' number of known command attributes: {}'.format(
+		len(shell.commands.arguments.arghist)),
+		'root modules and packages']
+	for m in shell.__all__:
+		mod = getattr(shell, m)
+		welcome.append(' {} at {}: version="{}"'.format(
+			mod.__name__, mod.__path__, mod.__version__))
+	del sqlalchemy
+	del html5lib
+	#shell.prompt.col_demo()
+	shell.prompt.display(welcome)
+	shell.run()
+
+
+if __name__=='__main__':
+	main()
+
+# https://rdfalchemy.readthedocs.org/en/latest/
+
+#TODO: namespace members autocomplete
+#TODO: implement rdf commands
+#TODO: handle command line parameters in here.
+#TODO: bash autocompletion
+#TODO: default command registration in interactive mode
+#TODO: parsing of remote resources (https://kask.eti.pg.gda.pl/redmine/projects/sova/repository/revisions/00951bd8e28d7bd58facb5a1da3a17ae9df115d4/raw/portalSubsystem/data/pizza.owl)
+#TODO: somehow wrap line if input gets too long. http://stackoverflow.com/questions/9468435/look-how-to-fix-column-calculation-in-python-readline-if-use-color-prompt
+
+#try to parse these ontologies from the internet:
+# https://owlverbalizer.googlecode.com/hg-history/8da67288d6be51f8d635f3bb347bacb1cf74f811/examples/tests.owl
+# http://smi-protege.stanford.edu/repos/protege/protege4/libraries/owlapi-extensions/trunk/src/test/resources/pizza01.owl
+# http://wise.vub.ac.be/ontologies/restaurant.owl
+
+
