f88ce911932c56c8ad4bb69a53beb4dde2bc2c87
Jakob Höper <jakobh@dariahst.(none)>
2013-12-18 17:38:21 +0100

    extended doku for root module

--- kathaireo/__init__.py
+++ kathaireo/__init__.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 """\
-An interactive interpreter can be started by calling 
+kathaireo
+=========
+Package structure
+-----------------
+**kathaireo** consists of three subpackages: :mod:`.shell` implements an interctive
+command-line interface, including colorized output and autocompletion.
+The :mod:`commands` package contains interpreter functionality like command recognition and
+input value validation, and both syntax specifications and handler functions of default
+commands. The :mod:`rdf` package puts a facade in front of the underlying RDF library,
+rdflib_, and provides additional information about the operating context it is used in,
+like the identifiers given to open RDF graphs in an active shell session or lists of
+resources used by graphs or their bound namespaces.
+
+.. _rdflib https://github.com/RDFLib/rdflib
+
+Shell mode
+----------
+An interactive interpreter can be started by calling
 :func:`.shell.run`. It can be equipped with additional custom
-commands for which handler functions are implemented. To write 
+commands for which handler functions are implemented. To write
 one of those, the :mod:`.rdf` package might be useful.
 
+
+Command definition
+------------------
 Registration of new commands and their handlers goes like in
 this (not really useful) example:
 ::
@@ -24,12 +44,12 @@ this (not really useful) example:
 		title = rdf.rdflib.term.Literal(title)
 		g.add((resource, rdf.rdflib.namespace.DC.title, title))
 
-The above example handler function is decorated by the 
+The above example handler function is decorated by the
 :func:`cmd_handler` decorator, pointing to the function
 :func:`.commands.register_handler`. Functions decorated
 like this are automatically copied to the :mod:`.commands.handlers`
 module namespace and registered for any command syntax defined
-within their docstring like demonstrated. Instead of the 
+within their docstring like demonstrated. Instead of the
 decorator, one might as well use:
 ::
 
@@ -39,10 +59,12 @@ decorator, one might as well use:
 Like this, the kathaireo shell can easily be extended. Arguments can
 be configured in a similar manner thanks to the :mod:`.commands.arguments`
 module.
+
+
 """
 
 __docformat__ = "restructuredtext en"
-__version__ = "0.0.1b-dev"
+__version__ = "0.0.11-dev"
 __all__ = ['rdf', 'commands', 'shell']
 
 import rdf
@@ -51,9 +73,9 @@ import shell
 
 # decorator for command handler functions
 cmd_handler=commands.register_handler
-"""Decorator for command handler functions. 
+"""Decorator for command handler functions.
 Functions decorated by this will be copied
-to the global namespace of the 
+to the global namespace of the
 :mod:`commands.handlers` module and optionally
 be registered for any command syntax declared
 within their docstring."""
